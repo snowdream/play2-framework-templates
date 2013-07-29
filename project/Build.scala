7ef5bda81c5554ce982d99aa25e96b39f7b16b5b
snowdream <yanghui1986527@gmail.com>
2013-07-29 15:15:43 +0800

    [Add] add the plugin play2-war-plugin

--- project/Build.scala
+++ project/Build.scala
@@ -1,19 +1,25 @@
 import sbt._
 import Keys._
 import play.Project._
+import com.github.play2war.plugin._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "ssdemo-java"
+    val appName         = "play2-framework-templates"
     val appVersion      = "1.0"
 
     val appDependencies = Seq(
 	javaCore,
         "securesocial" %% "securesocial" % "master-SNAPSHOT"
+        
     )
 
-    val main = play.Project(appName, appVersion, appDependencies).settings(
+    val main = play.Project(appName, appVersion, appDependencies).settings(Play2WarPlugin.play2WarSettings: _*).settings(
 	resolvers += Resolver.url("sbt-plugin-snapshots", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns)
+    ).settings(
+    // ... Your own settings here
+    Play2WarKeys.servletVersion := "2.5",
+    Play2WarKeys.disableWarningWhenWebxmlFileFound :=true
     )
 
 }
